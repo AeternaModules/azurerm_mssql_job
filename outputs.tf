852279c774fead049c3e7e85939522bd3f4a5cd0
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "mssql_jobs" {
   description = "All mssql_job resources"
   value       = azurerm_mssql_job.mssql_jobs
 }
+output "mssql_jobs_description" {
+  description = "List of description values across all mssql_jobs"
+  value       = [for k, v in azurerm_mssql_job.mssql_jobs : v.description]
+}
+output "mssql_jobs_job_agent_id" {
+  description = "List of job_agent_id values across all mssql_jobs"
+  value       = [for k, v in azurerm_mssql_job.mssql_jobs : v.job_agent_id]
+}
+output "mssql_jobs_name" {
+  description = "List of name values across all mssql_jobs"
+  value       = [for k, v in azurerm_mssql_job.mssql_jobs : v.name]
+}
 
